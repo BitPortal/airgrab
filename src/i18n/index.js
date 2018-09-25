3f7b4c163113489919d5443678e1704f35cdcc54
wangsitu <996803783@qq.com>
2018-09-25 11:35:19 +0800

    增加国际化i18n

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,11 +1,9 @@
 import Vue from 'vue';
 import VueI18n from 'vue-i18n';
-
 import zh from './lang/zh';
 import en from './lang/en';
-Vue.use(VueI18n);
 
-console.log(zh);
+Vue.use(VueI18n);
 
 const i18n = new VueI18n({
   locale: 'zh',
